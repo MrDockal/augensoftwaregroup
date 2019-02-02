ffad1cb15fa6ce167988b8a82f92574dbfdff501
Jan Dočkal <dockal07@gmail.com>
2019-02-02 16:41:08 +0100

    Pagination default limit to 20 records

--- src/Server/paginationFilter/paginationFilter.ts
+++ src/Server/paginationFilter/paginationFilter.ts
@@ -1,11 +1,11 @@
 import { IPagination } from "../../Model/IPagination";
 
-const OFFSET = 15;
+const LIMIT = 20;
 export const paginationFilter = <T extends any>(data: T[], page: number = 1): IPagination<T> => {
-	const slicedData = data.slice(page * OFFSET - OFFSET, page * OFFSET);
+	const slicedData = data.slice(page * LIMIT - LIMIT, page * LIMIT);
 	return {
 		data: slicedData,
-		pageCount: Math.ceil(data.length / OFFSET),
+		pageCount: Math.ceil(data.length / LIMIT),
 		page
 	};
 };
