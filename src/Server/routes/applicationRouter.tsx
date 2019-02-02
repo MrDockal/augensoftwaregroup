f3819759fb0ef871c7ec745abc402fad73f269b6
Jan Dočkal <dockal07@gmail.com>
2019-02-02 16:03:46 +0100

    Allow server to render styles

--- src/Server/routes/applicationRouter.tsx
+++ src/Server/routes/applicationRouter.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import App, { IAppProps } from '../../Web/App';
+import App from '../../Web/App';
 import * as express from 'express';
 import { renderToString } from 'react-dom/server';
 import { html } from '../html';
 import * as cors from 'cors';
 import { IRepository } from '../repository/IRepository';
 import { customerRoutes } from './customer/customerRoutes';
+import { ServerStyleSheet } from 'styled-components';
 
 export const createApplicationRouter = (
 	app: express.Application,
@@ -17,18 +18,13 @@ export const createApplicationRouter = (
 
 	const apiRoute = '/api/v1';
 	const reactAppRouting = (_req: express.Request, res: express.Response) => {
-		const appProps: IAppProps = {
-			config: {
-				baseURL: apiRoute,
-			}
-		};
-		const body = renderToString(React.createElement<any>(
-			App,
-			appProps
-		));
+		const sheet = new ServerStyleSheet();
+		const body = renderToString(sheet.collectStyles(<App config={{baseURL: apiRoute}} />));
+		const styles = sheet.getStyleTags();
 		res.send(
 			html({
-				body
+				body,
+				styles,
 			})
 		);
 	};
